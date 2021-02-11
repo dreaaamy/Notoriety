f5b95896ed86456d1a9c3b52854d76dc9a9a08fc
a <78894255+dreaaamy@users.noreply.github.com>
2021-02-11 13:25:35 +0530

    Add files via upload

---
+++ notarieety autofarm.lua
@@ -0,0 +1,17 @@
+--Speed (Decrease this if you are getting killed)
+speed = 35
+
+--Time before game force restart(In second)
+killtime = 360
+
+--Item to ignore
+ignore = {'Money'}
+
+--Grab safe box or not(Default true)
+grabsafe = true
+
+--Item that is in safe
+safeitem = {'JewelCrown','RingBox','Money','Coke'}
+
+--loadstring
+loadstring(game:HttpGet("https://raw.githubusercontent.com/CallMeChin/CallMeChin.github.io/main/roblox%20scripts/Notoriety%20Auto%20Farm", true))()
